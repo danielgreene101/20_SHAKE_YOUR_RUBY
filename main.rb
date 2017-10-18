7335fb3b848dcef153463923f122b7223a6ff8fb
DanielGreene101 <d.b.greene101@gmail.com>
2017-10-18 14:32:46 -0500

    finished

---
+++ main.rb
@@ -0,0 +1,178 @@
+flowers = ["Everybody clap", "Moonwalk", "Shopping Cart", "Robot", "Twist", "Cabbage Patch", "Raise the roof", "Sprinkler", "Running Man", "Worm", "Shake your Ruby"]
+
+e = flowers.to_enum
+
+3.times do
+	puts e.next
+end
+
+e.rewind.next
+1.times do
+	puts e.next
+end
+e.rewind
+
+puts e.next 
+
+2.times do
+	e.next
+end
+
+puts e.next
+puts e.next
+
+e.rewind
+
+3.times do
+	e.next
+end
+
+puts e.next
+
+e.rewind
+
+puts e.next
+
+e.rewind
+
+10.times do
+	e.next
+end
+puts e.next
+
+e.rewind
+
+puts e.next
+
+6.times do
+	e.next
+end
+
+puts e.next
+
+
+e.rewind
+
+6.times do
+	e.next
+end
+
+puts e.next
+
+e.rewind
+
+8.times do
+	e.next
+end
+
+puts e.next
+
+e.rewind
+
+5.times do
+	e.next
+end
+
+puts e.next
+
+e.rewind
+
+9.times do
+	e.next
+end
+
+puts e.next
+
+e.rewind
+puts e.next
+
+9.times do
+	e.next
+end
+
+puts e.next
+
+e.rewind
+puts e.next
+
+e.rewind
+1.times do
+	e.next
+end
+puts e.next
+
+e.rewind
+
+9.times do
+	e.next
+end
+
+puts e.next
+
+e.rewind
+
+3.times do
+	e.next
+end
+puts e.next
+
+e.rewind
+
+7.times do
+	e.next
+end
+puts e.next
+
+e.rewind
+puts e.next
+
+9.times do
+	e.next
+end
+puts e.next
+
+e.rewind
+puts e.next
+
+
+e.rewind
+
+2.times do
+	e.next
+end
+puts e.next
+
+5.times do
+	e.next
+end
+puts e.next
+
+e.rewind
+
+6.times do
+	e.next
+end
+puts e.next
+
+e.rewind
+
+4.times do
+	e.next
+end
+puts e.next
+
+5.times do
+	e.next
+end
+puts e.next
+
+e.rewind
+puts e.next
+
+9.times do
+	e.next
+end
+puts e.next
+
+
+# Shake your Ruby
